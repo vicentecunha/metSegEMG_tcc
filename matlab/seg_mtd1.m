d15ad869506ff2eee3fadf699f28eadf9086dc19
Vicente <vicente.cunha@tet.com>
2015-09-29 13:39:30 -0300

    Trabalhando no MTD1

--- matlab/seg_mtd1.m
+++ matlab/seg_mtd1.m
@@ -1,51 +1,44 @@
-%%
-% MTD1 - método iterativo utilizando thresholding para detecção de centros
-% de segmentos de comprimento constante
-%
-%   Argumentos:
-%   x - matriz column-wise com os sinais a serem segmentados
-%   l - comprimento desejado para os segmentos
-%   q - razão de atualização entre iterações para valor de threshold
-%   r_target - ..
-%   T_lim - valor de limite inferior para Threshold
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                             %
+% MTD1 - método iterativo utilizando thresholding para detecção de centros    %
+% 	de segmentos de comprimento constante                                     %
+%                                                                             %
+% Argumentos:                                                                 %
+%   x - matriz column-wise com os sinais a serem segmentados                  %
+%   l - comprimento desejado para os segmentos                                %
+%   q - razão de atualização entre iterações para valor de threshold          %
+%   r_target - razão mínima esperada entre número de segmentos e comprimento  %
+%		total de sinal                                                        %
+%   T_lim - valor de limite inferior para threshold                           %
+%                                                                             %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function x_seg = seg_mtd1(x, l, q, r_target, T_lim)
-    % Valor inicial de threshold corresponde aos máximos dos sinais
-    T0 = max(x);
-    % Obtém número de canais
-    [L, num_canais] = size(x);
+
+    T0 = max(x); % Máximo dos canais é o valor inicial de threshold
+    [L, num_channels] = size(x); % Obtém comprimento de sinais e número de canais
         
-    % Realiza segmentação para cada um dos canais
-    for current_channel = 1:num_canais
-        % Threshold inicial
-        T_k = T0(current_channel);
+    for current_channel = 1:num_canais % Segmentação para cada um dos canais
+
+		T_k = T0(current_channel); % Threshold inicial
         
-        % Variável lógica para determinar o final do método iterativo
+		% Processo iterativo:
         target_reached = false;
-        while ~target_reached
-            % Calcula threshold desta iteração
-            T_k = q*T_k;
-            % Limite de valor de threshold atingido
-            if T_k < T_lim
-                break;
+		while ~target_reached
+		
+            T_k = q*T_k; % Calcula threshold desta iteração
+            
+            if T_k < T_lim % Limite de valor de threshold atingido
+                break
             end
                 
             % Identifica os possíveis candidatos para centros de segmentos
             [~, centers_locs_current_channel] = findpeaks(x(:,current_channel), ...
                'MinPeakHeight', T_k, 'MinPeakDistance',l);
-           
-            % Verificação do número de candidatos a centro identificados
-            if(length(centers_locs_current_channel) > N)
-                error('Número de possíveis centros identificados maior que o N fornecido.')
-            end
             
             % Determina o encerramento do processo iterativo
              target_reached = (length(centers_locs_current_channel)/L > N);
-        end
-        
-        % Armazena os centros de segmentos para o canal atual
-        index = zeros(length(centers_locs_current_channel),1);
-        index(:) = current_channel;
-        centers_locs = [centers_locs; index centers_locs_current_channel];
+
+		end
     end
 end
