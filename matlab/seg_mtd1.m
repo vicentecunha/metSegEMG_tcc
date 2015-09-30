e7817a64ff5f8f4d4aed1bbba2f8705a792c8759
Vicente <vicente.cunha@tet.com>
2015-09-30 16:49:17 -0300

    Finished MTD1.

--- matlab/seg_mtd1.m
+++ matlab/seg_mtd1.m
@@ -15,61 +15,57 @@
 
 function x_seg = seg_mtd1(x, l, q, r_target, T_lim)
 
-	% Máximo dos canais é o valor inicial de threshold
-    T0 = max(x); 
-	
 	% Obtém comprimento do sinal e número de canais
     [L, numberOfChannels] = size(x);
-	
-	% Array utilizado para armazenar o número de candidatos a centro de
-	% 	segmentos identificados por canal
-	numberOfCandidates = zeros(1, numberOfChannels); 
-	
-	% Cell array utilizado para armazenar as localizações de candidatos
-	%	a centro de segmentos identificados por canal
-	locsOfCandidates = cell(1, numberOfChannels);
-	
-    % Plot dos centros identificados
-    figure(1)
-    
-	% Identificação de candidatos a centro para cada um dos canais
-    for currentChannel = 1:numberOfChannels 
 
-		% Threshold inicial
-		T_k = T0(currentChannel); 
-        		
-		% Processo iterativo
-        targetReached = false;
-		while ~targetReached
-
-			% Calcula threshold desta iteração
-            T_k = q*T_k; 
-            
-			% Verifica se limite de valor de threshold foi atingido
-            if T_k < T_lim
-                warning(['Threshold limit reached on channel ', ...
-                    num2str(currentChannel), '. Stopping iterations.'])
-                break
-            end
-                
-            % Identifica candidatos para centros de segmentos
-            [centerValues, centerLocs] = findpeaks(x(:,currentChannel), ...
-               'MinPeakHeight', T_k, 'MinPeakDistance',l);
-            
-            % Determina o encerramento do processo iterativo
-             targetReached = (length(centerLocs)/L > r_target);
-			 
-		end
-		
-		% Armazena o número de candidatos identificados para este canal
-		numberOfCandidates(currentChannel) = (length(centerLocs));
-
-        % Armazena os locais dos candidatos identificados para este canal
-        locsOfCandidates{currentChannel} = centerLocs;
-        
-        % Plot dos centros identificados
-        subplot(5,2,currentChannel), plot(x(:,currentChannel)), hold on, ...
-            plot(centerLocs,centerValues,'r*'), hold off
-        
+    % Soma dos canais
+    x_sum = zeros(L,1);
+    for currentChannel = 1:numberOfChannels
+        x_sum = x_sum + x(:,currentChannel);
     end
+
+	% Valor inicial de threshold corresponde ao máximo do sinal
+    T_k = max(x_sum); 
+
+	% Processo iterativo
+    targetReached = false;
+	while ~targetReached
+	
+		% Calcula threshold desta iteração
+        T_k = q*T_k; 
+        
+		% Verifica se limite de valor de threshold foi atingido
+        if T_k < T_lim
+            warning('Threshold limit reached. Stopping iterations.')
+            break
+        end
+            
+        % Identifica candidatos para centros de segmentos
+        [centerValues, centerLocs] = findpeaks(x_sum, ...
+           'MinPeakHeight', T_k, 'MinPeakDistance',l);
+        
+        % Determina o encerramento do processo iterativo
+         targetReached = (length(centerLocs)/L > r_target);
+		 
+	end
+    
+    % Eliminação de centros que estão muito aos extremos do sinal
+    if(centerLocs(1) < l/2)
+        centerLocs(1) = [];
+        centerValues(1) = [];
+    end
+    if(L - centerLocs(end) < l/2)
+        centerLocs(end) = [];
+        centerValues(end) = [];
+    end
+    
+    % Segmentação dos canais
+    x_seg = cell(length(centerLocs),numberOfChannels);
+    for currentChannel = 1:numberOfChannels
+        for currentSegment = 1:length(centerLocs)
+            x_seg{currentSegment,currentChannel} = ...
+                x(centerLocs - l/2: centerLocs+l/2-1,currentChannel);
+        end
+    end
+	
 end
