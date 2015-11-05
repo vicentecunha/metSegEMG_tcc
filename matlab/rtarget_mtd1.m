3d275746eb2c46ca567c6d3e51cbfd0d2a2bae6e
Vicente <vicente.cunha@tet.com>
2015-11-05 17:58:20 -0200

    Reformatted tex document.

--- matlab/rtarget_mtd1.m
+++ matlab/rtarget_mtd1.m
@@ -1,14 +1,14 @@
 %%
-%   Determinação de r_target para MTD1
+%   Determinacao de r_target para MTD1
 %%
 
 %% Base de dados Ninapro
 
 ninaproList = ls('database/ninapro2/S*_E1*');
 numberOfSubjects = length(ninaproList);
-numberOfSegments = 102; % 17 movimentos * 6 repetições
+numberOfSegments = 102; % 17 movimentos * 6 repeticoes
 
-%% Obtenção de r_target da database
+%% Obtencao de r_target da database
 
 r_target = zeros(numberOfSubjects, 1);
 
@@ -16,13 +16,12 @@ for currentSubject = 1:numberOfSubjects
 
     fprintf('currentSubject = %i\n',currentSubject)
     
-    % Carrega o voluntário atual
+    % Carrega o voluntario atual
     load (['database/ninapro2/' ninaproList(currentSubject,:)])
     
-    % r_target para este voluntário
+    % r_target para este voluntario
     r_target(currentSubject) = numberOfSegments/length(emg);
     
 end
 
 min_r_target = min(r_target)
-max_r_target = max(r_target)
