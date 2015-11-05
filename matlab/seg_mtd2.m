f28f3978717b7a92868911dfc329472bd92f32c8
Vicente Cunha <vicentecunha1@hotmail.com>
2015-11-05 11:34:17 -0200

    Adicionei variacao de parametros do MTD2.

--- matlab/seg_mtd2.m
+++ matlab/seg_mtd2.m
@@ -22,74 +22,93 @@ function [x_seg, centerLocs] = seg_mtd2(x, l, A, B, C)
 
 %% Preprocessamento
 
-	% Obtem comprimento do sinal e numero de canais
-    [L, numberOfChannels] = size(x);
-    
-    % Retificacao de sinal
-    x_ret = abs(x);
-     
- 	% Suavizacao utilizando media movel
-	x_smooth = reshape(smooth(x_ret, 32), L, numberOfChannels);
-    
+% Obtem comprimento do sinal e numero de canais
+[L, numberOfChannels] = size(x);
+
+% Retificacao
+x_ret = abs(x);
+
+% Normalizacao
+x_norm = zeros(L, numberOfChannels);
+for currentChannel = 1:numberOfChannels
+    x_norm(:,currentChannel) = ...
+        x_ret(:,currentChannel)./max(x_ret(:,currentChannel));
+end
+
 %% Metodo
-    
-    % Cell array para armazenar posicoes dos segmentos identificados
-    centerLocsCell = cell(1,numberOfChannels);
 
-    for currentChannel = 1:numberOfChannels
-    
-        % Calculo do threshold
-        maxValue = max(x_smooth(:,currentChannel));
-        meanValue = mean(x_smooth(:,currentChannel));
-        if maxValue > (A*meanValue)
-            T = B*meanValue;
-        else
-            T = maxValue/C;
-        end
+% Cell array para armazenar posicoes dos segmentos identificados
+centerLocsCell = cell(1,numberOfChannels);
 
-        % Identifica centros de segmentos
-        [~, centerLocsCell{1,currentChannel}] = ...
-            findpeaks(double(x_smooth(:,currentChannel)), ...
-            'MinPeakHeight', T, 'MinPeakDistance',l);       
+for currentChannel = 1:numberOfChannels
+    
+    % Calculo do threshold
+    maxValue = 1; % sinais normalizados
+    meanValue = mean(x_norm(:,currentChannel));
+    if maxValue > (A*meanValue)
+        T = B*meanValue;
+    else
+        T = maxValue/C;
     end
-        
+    
+    % Identifica centros de segmentos
+    [~, centerLocsCell{1,currentChannel}] = ...
+        findpeaks(double(x_norm(:,currentChannel)), ...
+        'MinPeakHeight', T, 'MinPeakDistance',l);
+end
+
 %% Segmentacao dos canais
 
-    % Maximo numero de segmentos detectados
-    numberOfSegments = 0;
-    for currentChannel = 1:numberOfChannels        
-        currentChannelNumberOfSegments = length(centerLocsCell{1,currentChannel});        
-        if  currentChannelNumberOfSegments > numberOfSegments
-            numberOfSegments = currentChannelNumberOfSegments;
-        end        
+% Maximo numero de segmentos detectados
+numberOfSegments = 0;
+for currentChannel = 1:numberOfChannels
+    currentChannelNumberOfSegments = length(centerLocsCell{1,currentChannel});
+    if  currentChannelNumberOfSegments > numberOfSegments
+        numberOfSegments = currentChannelNumberOfSegments;
     end
-    
-    % Clustering dos centros de segmentos detectados
-    centerLocsArray = cell2mat(centerLocsCell');
-    idx = kmeans(centerLocsArray,numberOfSegments);
-    
-    % Media dos clusters
-    centerLocsMean = zeros(numberOfSegments,1);
-    for currentCluster = 1:numberOfSegments
-        centerLocsMean(currentCluster) = ...
-            mean(centerLocsArray(idx == currentCluster));
-    end
-    centerLocs = sort(round(centerLocsMean));
-    
-    % Segmentacao
-    x_seg = cell(numberOfSegments,numberOfChannels);
-    for currentChannel = 1:numberOfChannels
-        for currentSegment = 1:numberOfSegments
-            if mod(l,2) == 0 % se l for par
+end
+
+% Clustering dos centros de segmentos detectados
+centerLocsArray = cell2mat(centerLocsCell');
+idx = kmeans(centerLocsArray,numberOfSegments);
+
+% Clustering dos centros de segmentos detectados
+centerLocsArray = cell2mat(centerLocsCell');
+[~,C] = kmeans(centerLocsArray,numberOfSegments);
+centerLocs = sort(round(C));
+
+% Segmentacao
+x_seg = cell(numberOfSegments,numberOfChannels);
+for currentChannel = 1:numberOfChannels
+    for currentSegment = 1:numberOfSegments
+        if mod(l,2) == 0 % se l for par
+            if (centerLocs(currentSegment)-l/2) < 1 % segmento muito a esquerda
                 x_seg{currentSegment,currentChannel} = ...
-                    x(centerLocs(currentSegment)-l/2: ...
-                    centerLocs(currentSegment)+l/2 - 1, currentChannel);
-            else % se l for impar
+                    x(1:centerLocs(currentSegment)+l/2 - 1, currentChannel);
+            else if (centerLocs(currentSegment)+l/2 - 1) > L % segmento muito a direita
+                    x_seg{currentSegment,currentChannel} = ...
+                        x(centerLocs(currentSegment)-l/2:L, currentChannel);
+                else
+                    x_seg{currentSegment,currentChannel} = ...
+                        x(centerLocs(currentSegment)-l/2: ...
+                        centerLocs(currentSegment)+l/2 - 1, currentChannel);
+                end
+            end
+        else % se l for impar
+            if (centerLocs(currentSegment)-(l+1)/2) < 1 % segmento muito a esquerda
                 x_seg{currentSegment,currentChannel} = ...
-                    x(centerLocs(currentSegment)-(l+1)/2: ...
-                    centerLocs(currentSegment)+(l+1)/2 - 1, currentChannel);
+                    x(1:centerLocs(currentSegment)+l/2 - 1, currentChannel);
+            else if (centerLocs(currentSegment)+(l+1)/2 - 1) > L % segmento muito a direita
+                    x_seg{currentSegment,currentChannel} = ...
+                        x(centerLocs(currentSegment)-l/2:L, currentChannel);
+                else
+                    x_seg{currentSegment,currentChannel} = ...
+                        x(centerLocs(currentSegment)-(l+1)/2: ...
+                        centerLocs(currentSegment)+(l+1)/2 - 1, currentChannel);
+                end
             end
         end
     end
-    
+end
+
 end
